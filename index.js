7af2d5564d60994182715fe5317ecfce3ec5c354
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2021-08-03 16:28:27 +0200

    Reading pages

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const toolbarOptions = [
-  ['hamburger', 'bold', 'italic', 'underline'],        // toggled buttons
+const { Subject, interval, fromEvent, combineLatest } = rxjs;
+const { debounceTime, skip, take, concatWith, mergeWith } = rxjs.operators;
 
-  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-  [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+// create ready event from dom and alpline
+const ready = combineLatest([
+  fromEvent(window, 'DOMContentLoaded')
+]).pipe(take(1));
 
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
-
-  ['clean'],                                         // remove formatting button
-];
-
+// Create quill editor
 const quill = new Quill('#editor', {
   theme: 'snow',
   modules: {
@@ -20,82 +14,116 @@ const quill = new Quill('#editor', {
   }
 });
 
-function ready(fn) {
-  if (document.readyState !== 'loading') {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-// grist.docApi.applyUserActions([ ['UpdateRecord', tableId, rowId, {
-//           [colId]: data
-//         }]])
-
-ready(() => {
+// Subscribe to grist data
+ready.subscribe(() => {
   grist.ready();
-  grist.onRecords(function(records) {
-    const store = Alpine.store('pages');
-    store.list = records;
-    console.log("Records", records);
-  });
-  grist.onRecord(function(record) {
-    console.log("On record", record);
-  })
-  grist.on('message', async (msg) => {
-    if (msg.tableId === 'Pages') {
-      const folders = toObjects(await grist.docApi.fetchTable("Folders"));
-      const store = Alpine.store('folders');
-      store.list = folders;
-      console.log("Folders", folders);
+  grist.onRecords(async function(pages) {
+    // Every time pages change - update pages and folders
+    const pagesStore = Alpine.store('pages');
+    pagesStore.list = pages;
+    const folders = toObjects(await grist.docApi.fetchTable("Folders"));
+    const folderStore = Alpine.store('folders');
+    folderStore.list = folders;
+    if (!folderStore.selected() && folders.length) {
+      folderStore.id = folders[0].id;
     }
-  })
+    if (folderStore.selected()) {
+      pagesStore.folderSelected(folderStore.selected());
+    }
+  });
 });
 
-document.addEventListener('alpine:init', () => {
+// create events emitted from the ui
+const folderClick = new Subject();
+const pageClick = new Subject();
+const textChanged = new Subject();
+const nameChanged = new Subject();
+quill.on('text-change', () => textChanged.next(null));
+// Throttle the save event, to not occur more often than once every second.  
+const saveEvent = ready
+  .pipe(concatWith(textChanged), skip(1)) // emit after ready
+  .pipe(mergeWith(nameChanged)) // listen also to name change
+  .pipe(debounceTime(1000)); // buffer for 1 second
+
+const addPage = new Subject();
+const addFolder = new Subject();
+
+let menu, folders, pages;
+
+// create store
+fromEvent(document, 'alpine:init').subscribe(() => {
   Alpine.store('menu', {
     on: true,
-    page : 0,
-    folder: 0,
-    pages : [],
     toggle() {
-      this.on = !this.on
-    },
-    showFolder(id) {
-      this.folder = id;
-      const folder = Alpine.store('folders').list.find(x => x.id === id);
-      this.pages = Alpine.store('pages').list.filter(x=> x.Folder === folder.Name);
+      this.on = !this.on;
     }
   });
-  Alpine.store('page', {
-    id: 0,
-    text: '',
-    save() {
-
-    }
-  });
-  Alpine.store('folders', {
+  folders = Alpine.store('folders', {
     list: [],
-    add() {
-      this.on = !this.on
-    },
-    remove() {
-
+    id: 0,
+    selected() {
+      return this.id ? this.list.find(f => f.id === this.id) : null;
     }
   });
   Alpine.store('pages', {
     list: [],
-    add() {
-      this.on = !this.on
-    },
-    remove() {
-
-    },
-    rename() {
-
-    },
+    active: [],
+    id: 0,
+    current: { Name: '' },
+    folderSelected(folder) {
+      this.active = this.list.filter(p => p.Folder === folder.Name);
+    }
   });
+  menu = Alpine.store('menu');
+  pages = Alpine.store('pages');
+  folders = Alpine.store('folders');
 })
+
+// Handle ui events
+folderClick.subscribe(id => {
+  folders.id = id;
+  const folder = folders.list.find(f => f.id === id);
+  pages.active = pages.list.filter(p => p.Folder === folder.Name);
+});
+
+pageClick.subscribe(id => {
+  const activePage = pages.active.find(p => id === id);
+  pages.current = activePage;
+  pages.id = id;
+  if (activePage.Text) {
+    quill.setContents(JSON.parse(activePage.Text));
+  } else {
+    quill.setContents(null);
+  }
+})
+
+saveEvent.subscribe(async () => {
+  if (!pages.id) return;
+  const content = quill.getContents();
+  await grist.docApi.applyUserActions([['UpdateRecord', 'Pages', pages.id, {
+    "Text": JSON.stringify(content),
+    "Name": pages.current.Name,
+  }]]);
+});
+
+addFolder.subscribe(async () => {
+  const lastId = folders.list[folders.list.length - 1].id;
+  const name = `Folder${lastId + 1}`;
+  await grist.docApi.applyUserActions([['AddRecord', 'Folders', null, {
+    "Name": name,
+  }]]);
+  const list = toObjects(await grist.docApi.fetchTable("Folders"));
+  folders.list = list;
+});
+
+addPage.subscribe(async () => {
+  const lastId = pages.list.length ? pages.list[pages.list.length - 1].id : '0';
+  const name = `Untitled${lastId + 1}`;
+  await grist.docApi.applyUserActions([['AddRecord', 'Pages', null, {
+    "Name": name,
+    "Folder": folders.id
+  }]]);
+});
 
 function toObjects(data) {
   const keys = Object.keys(data).filter(k => Array.isArray(data[k]));
